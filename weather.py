8c075bebb8c55f51392516ff386039aa62910db0
Casey Warren <caseywar1017@gmail.com>
2022-08-17 12:25:14 -0400

    python practice

---
+++ weather.py
@@ -0,0 +1,28 @@
+# temp = 75
+# if temp > 80:
+#     print("Its too hot")
+#     print("Stay inside!")
+# print("Have a good Day")
+
+# temp = 75
+# if temp > 80:
+#     print("Its too hot")
+#     print("Stay inside!")
+# else:
+#     print("Enjoy the outdoors")
+
+# temp = 70
+# if temp > 80:
+#     print("Its too hot")
+#     print("Stay inside!")
+# elif temp < 60:
+#     print("Its too cold")
+#     print("Stay inside")
+# else:
+#     print("Enjoy the outdoors")
+
+temp = 22
+if temp > 80 or temp < 60:
+    print("STAY INSIDE")
+else:
+    print("Enjoy the outdoors <3")
